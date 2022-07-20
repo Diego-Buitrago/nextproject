800f85733964dbef27ac536d5ed73f6d88d4f281
Diego-pavas <diego.buitrago@pavas.co>
2022-07-20 11:56:51 -0500

    inicial

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Container from "../components/Container"
+
+const About = () => {
+    return (
+        <Container>
+            <h1>About</h1>
+            <p>Qui amet minim velit exercitation amet tempor qui nostrud et anim amet aliqua eu. Dolor adipisicing voluptate occaecat proident anim consequat commodo Lorem officia officia sit. Esse quis amet ut in cillum non veniam ipsum in. Irure ex reprehenderit aute laboris sunt velit consectetur aute laborum irure. Commodo cupidatat ad labore et cupidatat deserunt non ad. Qui pariatur labore elit adipisicing ullamco enim.</p>
+        </Container>
+    )
+}
+
+export default About
---
+++ pages/services.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Container from "../components/Container"
+
+const Services = () => {
+    return (
+        <Container>  
+             <Head>
+                <title>Next.js Projet - Services</title>
+            </Head>          
+            <h1>Services</h1>
+            <p>Eiusmod laborum labore nostrud amet anim Lorem sunt irure. Veniam enim est incididunt cupidatat nisi culpa ea ea veniam sunt dolore exercitation sint fugiat. Fugiat Lorem incididunt officia magna enim sunt exercitation dolor quis cillum esse labore et.</p>
+        </Container>
+    )
+}
+
+export default Services
